59ea47853e4ea4422925b332a81dc2380a65552b
SiWa123571113 <64312633+SiWa123571113@users.noreply.github.com>
2022-05-29 10:38:21 +0200

    added CPU-Temp reading

--- logging.sh
+++ logging.sh
@@ -4,7 +4,7 @@ R1=`cat /sys/class/net/$1/statistics/rx_bytes`
 T1=`cat /sys/class/net/$1/statistics/tx_bytes`
 
 echo "" >> log_1.txt
-echo "date: CPU%, Mem%, Net_in(rx), Net_out(tx)" >> log_1.txt
+echo "date, CPU%, Temp°, Mem%, Net_in(rx), Net_out(tx)" >> log_1.txt
 
 while true
 do
@@ -16,7 +16,8 @@ do
 	CPU_usage=`top -b -n1 | fgrep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`
 	Mem_per=`free -m | awk 'NR==2{printf "%.2f", $3*100/$2}'`
 	time=$(date "+%Y-%m-%d %H:%M:%S")
-	echo "${time}: ${CPU_usage}, ${Mem_per}, ${Net_in}, ${Net_out}" >> log_1.txt
+	temp=`sensors -j coretemp-isa-0000 | jq -r  '.["coretemp-isa-0000"] | .["Package id 0"] | .["temp1_input"]'`
+	echo "${time}, ${CPU_usage}, ${temp}, ${Mem_per}, ${Net_in}, ${Net_out}" >> log_1.txt
 	R1=$R2
 	T1=$T2
 	wait
