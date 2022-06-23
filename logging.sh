c14edd20318abc48828408d5579347c4bd15e7a8
SiWa123571113 <64312633+SiWa123571113@users.noreply.github.com>
2022-06-23 11:17:20 +0200

    added error message

--- logging.sh
+++ logging.sh
@@ -1,12 +1,18 @@
 ##!/bin/bash
 
+if [ $# -eq 0 ]
+then
+ echo "Missing first Argument (network insterface), exiting"
+ exit -1
+fi
+
 R1=`cat /sys/class/net/$1/statistics/rx_bytes`
 T1=`cat /sys/class/net/$1/statistics/tx_bytes`
 time=$(($(date +%s%N)/1000000))
 f=`echo "log_${time}.csv"`
 
 echo "" >> $f
-echo "date, CPU%, Temp°, Mem%, Net_in(rx), Net_out(tx), clients, uptime, Server_in(rx), Server_out(tx)" >> $f
+echo "date, CPU%, Temp°, Mem%, Net_in(rx), Net_out(tx)" >> $f
 
 while true
 do
@@ -19,9 +25,7 @@ do
 	Mem_per=`free -m | awk 'NR==2{printf "%.2f", $3*100/$2}'`
 	time=$(date "+%Y-%m-%d %H:%M:%S")
 	temp=`sensors -j coretemp-isa-0000 | jq -r  '.["coretemp-isa-0000"] | .["Package id 0"] | .["temp1_input"]'`
-	Server_info=`python3 get_server_info.py`
-	
-	echo "${time}, ${CPU_usage}, ${temp}, ${Mem_per}, ${Net_in}, ${Net_out}, ${Server_info}" >> $f
+	echo "${time}, ${CPU_usage}, ${temp}, ${Mem_per}, ${Net_in}, ${Net_out}" >> $f
 	R1=$R2
 	T1=$T2
 	wait
