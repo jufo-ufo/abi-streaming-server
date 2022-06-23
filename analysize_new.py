d4fc3d484475b372ba47d9662f40b21ec505c2ec
AlexanderHD27 <alexander.wersching@t-online.de>
2022-06-23 13:38:03 +0200

    Added new colums to plot

--- analysize_new.py
+++ analysize_new.py
@@ -3,6 +3,7 @@ import sys
 import requests
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+
 import pandas as pd
 import sys
 import time
@@ -16,43 +17,50 @@ def animate(i):
     except:
         print("Invalid first argument (logfile), exiting...")
         exit()
-    
-    print(data["date"])
     data["date"] = pd.to_datetime(data["date"])
 
     x = data["date"]
 
-    exit()
 
-    plt.tight_layout()
-
-    plt.subplot(2, 2, 1)
+    plt.subplot(2, 3, 1)
     plt.cla()
     plt.grid()
     plt.ylim(0, 100)
     plt.plot(x, data[" CPU%"], label='CPU in %', linewidth=0.75, color="mediumblue")
     plt.legend(loc='upper left')
 
-    plt.subplot(2, 2, 2)
+    plt.subplot(2, 3, 2)
     plt.cla()
     plt.grid()
     plt.ylim(0, 100)
     plt.plot(x, data[" Mem%"], label='Memory in %', linewidth=1, color="green")
     plt.legend(loc='upper left')
 
-    plt.subplot(2, 2, 3)
+    plt.subplot(2, 3, 3)
     plt.cla()
     plt.grid()
     plt.plot(x, data[" Net_out(tx)"], label='TX in kB/s', linewidth=0.75, color="darkorange")
     plt.plot(x, data[" Net_in(rx)"], label='RX in kB/s', linewidth=0.75, color="royalblue")
     plt.legend(loc='upper left')
 
-
-    plt.subplot(2, 2, 4)
+    plt.subplot(2, 3, 4)
     plt.cla()
     plt.grid()
     plt.ylim(-5, 120)
     plt.plot(x, data[" Temp°"], label='Temp in °C', linewidth=1, color="darkred")
+    plt.legend(loc='upper left')
+
+    plt.subplot(2, 3, 5)
+    plt.cla()
+    plt.grid()
+    plt.plot(x, data[" Server_out(tx)"], label='Nginx TX in kB/s', linewidth=0.75, color="darkorange")
+    plt.plot(x, data[" Server_in(rx)"], label='Nginx RX in kB/s', linewidth=0.75, color="royalblue")
+    plt.legend(loc='upper left')
+
+    plt.subplot(2, 3, 6)
+    plt.cla()
+    plt.ylim(0, 200)
+    plt.plot(x, data[" clients"], label='Clients', linewidth=1, color="darkred")
     plt.legend(loc='upper left')
 
     plt.tight_layout()
@@ -61,6 +69,6 @@ def animate(i):
 ani = FuncAnimation(plt.gcf(), animate, interval=500)
 #animate(0)
 
-mng = plt.get_current_fig_manager()
+#mng = plt.get_current_fig_manager()
 #mng.window.showMaximized()
 plt.show()
