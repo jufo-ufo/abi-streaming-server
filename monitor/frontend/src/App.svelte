35153f8cf41b94b2e1067a2ca2bf5d8addb7358b
AlexanderHD27 <alexander.wersching@t-online.de>
2022-06-26 14:58:53 +0200

    Setup frontend stuff

---
+++ monitor/frontend/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import TimeSeriesPlotCard from "./TimeSeriesPlotCard.svelte";
+    import ServerInfoCard from "./ServerInfoCard.svelte";
+</script>
+
+<style lang="css">
+main {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    gap: 1rem;
+
+    height: 100%;
+    width: 100%;
+}
+</style>
+
+<main>
+    <TimeSeriesPlotCard titel="CPU"/>
+    <TimeSeriesPlotCard titel="Network"/>
+    <ServerInfoCard/>
+    <TimeSeriesPlotCard titel="Memory/Swap"/>
+    <TimeSeriesPlotCard titel="Bandwidth"/>
+    <TimeSeriesPlotCard titel="Client Count/Acceped"/>
+    <TimeSeriesPlotCard titel="Disk"/>
+    <TimeSeriesPlotCard titel="Core Temperatur"/>
+</main>
---
+++ monitor/frontend/src/ServerInfoCard.svelte
@@ -0,0 +1,17 @@
+
+<style lang="scss">
+    @use "styles/global";
+    
+    div {
+		@include global.card;	
+        grid-row: span 2 /auto;
+    }
+
+    h1 {
+        margin: 0;
+    }
+</style>
+
+<div>
+    <h1>Server Info</h1>
+</div>
